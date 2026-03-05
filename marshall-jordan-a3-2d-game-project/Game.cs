abb40203108613a52e3ff5114e46bbd3351e3225
Jordan <jordan.marshall1@mohawkcollege.ca>
2026-03-05 13:55:40 -0500

    Spaced out the cards, added card, timer, and points placeholders

--- marshall-jordan-a3-2d-game-project/Game.cs
+++ marshall-jordan-a3-2d-game-project/Game.cs
@@ -9,25 +9,48 @@ namespace MohawkGame2D
     {
         // Place your variables here:
         Texture2D sword;
-        Texture2D heart;
+        Texture2D sword2;
+        Texture2D sword3;
+        Texture2D sword10;
         Texture2D back;
 
         public void Setup()
         {
             Window.SetTitle("Matching Game");
             Window.SetSize(800, 600);
-            sword = Graphics.LoadTexture("../../../../assets/graphics/sword.png");
-            heart = Graphics.LoadTexture("../../../../assets/graphics/heart.png");
-            back = Graphics.LoadTexture("../../../../assets/graphics/back.png");
+            sword = Graphics.LoadTexture("C:/Users/Jordan/source/repos/marshall-jordan-a3-2d-game-project/assets/graphics/sword01.png");
+            sword2 = Graphics.LoadTexture("C:/Users/Jordan/source/repos/marshall-jordan-a3-2d-game-project/assets/graphics/sword02.png");
+            sword3 = Graphics.LoadTexture("C:/Users/Jordan/source/repos/marshall-jordan-a3-2d-game-project/assets/graphics/sword03.png");
+            sword10 = Graphics.LoadTexture("C:/Users/Jordan/source/repos/marshall-jordan-a3-2d-game-project/assets/graphics/sword10.png");
+            back = Graphics.LoadTexture("C:/Users/Jordan/source/repos/marshall-jordan-a3-2d-game-project/assets/graphics/back.png");
         }
 
         public void Update()
         {
             Window.ClearBackground(Color.OffWhite);
-            Draw(back, 300, 300);
+            Text.Draw("TIMER", 350, 25); // Timer
+            Text.Draw($"POINTS: ", 600, 25); // Points
+            Draw(back, 75, 75);
+            Draw(back, 250, 75);
+            Draw(back, 425, 75);
+            Draw(back, 600, 75);
+
+            Draw(back, 75, 250);
+            Draw(back, 250, 250);
+            Draw(back, 425, 250);
+            Draw(back, 600, 250);
+
+            Draw(back, 75, 425);
+            Draw(back, 250, 425);
+            Draw(back, 425, 425);
+            Draw(back, 600, 425);
+
             if (Input.IsMouseButtonDown(0))
             {
-                Draw(sword, 300, 300);
+                Draw(sword, 75, 75);
+                Draw(sword2, 250, 75);
+                Draw(sword3, 425, 75);
+                Draw(sword10, 600, 75);
             }
         }
 
