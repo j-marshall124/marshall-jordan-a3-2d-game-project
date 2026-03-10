bcf8220a71cfbb04328d212c14ed387c0f26d854
j-marshall124 <jordan.marshall1@mohawkcollege.ca>
2026-03-10 15:36:31 -0400

    initial commit

---
+++ marshall-jordan-a3-2d-game/Game.cs
@@ -0,0 +1,24 @@
+﻿// Include the namespaces (code libraries) you need below.
+using System;
+using System.Numerics;
+
+// The namespace your code is in.
+namespace MohawkGame2D
+{
+    public class Game
+    {
+        // Place your variables here:
+
+
+        public void Setup()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+    }
+
+}
---
+++ marshall-jordan-a3-2d-game/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MohawkGame2D
+{
+    public class Player
+    {
+        Texture2D playerFrame1;
+        Texture2D playerFrame2;
+        Texture2D playerHurt;
+
+        public void PlayerLoad()
+        {
+            playerFrame1 = Graphics.LoadTexture("C:/Users/lifew/source/repos/marshall-jordan-a3-robot-game/assets/graphics/robot character1.png");
+            playerFrame2 = Graphics.LoadTexture("C:/Users/lifew/source/repos/marshall-jordan-a3-robot-game/assets/graphics/robot character2.png");
+            playerHurt = Graphics.LoadTexture("C:/Users/lifew/source/repos/marshall-jordan-a3-robot-game/assets/graphics/robot character hurt1.png");
+        }
+    }
+}
